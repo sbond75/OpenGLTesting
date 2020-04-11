518bb95d1b15a647f7b2e0628618e44c60828e1a
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-04-11 10:45:29 -0500

    Add project files.

---
+++ main.c
@@ -0,0 +1,21 @@
+// badprog.com
+#include <SDL/SDL.h>
+#include <GL/glew.h>
+
+void displayMe(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.5, 0.5, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	
+    return 0;
+}
