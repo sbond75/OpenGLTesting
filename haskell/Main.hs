528a3ea64c869cb0c1c486431b65646e4251888d
Ben Siraphob <bensiraphob@gmail.com>
2020-04-12 23:32:55 +0700

    Demonstrate abstraction of immediate mode

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -7,6 +7,11 @@ Driver -- examples:
 - NVidia, AMD, Intel, Microsoft software rendering for opengl 1.1
 OS - set up a window, how to init OpenGL in the window
 (create context),
+
+Buffer - location of memory to put data
+Color buffers - a buffer of RGB
+
+Immediate mode - commands are issued between glBegin and glEnd
 -}
 
 module Main where
@@ -25,15 +30,24 @@ import Control.Monad.Loops
 import GHC.Ptr
 import SDL.Event
 
-displayMe = do glClear GL_COLOR_BUFFER_BIT
-               glBegin GL_POLYGON
-               glVertex3f 0.0 0.0 0.0
-               glVertex3f 0.5 0.0 0.0
-               glVertex3f 0.5 0.5 0.0
-               glVertex3f 0.0 0.5 0.0
-               glEnd
-               glFlush
+-- Run glBegin with a GL enum, a body consisting of immediate mode
+-- commands, and wrap it between calls to glBegin and glEnd
+withGLMode :: MonadIO m => GLenum -> m a -> m ()
+withGLMode mode body = do glBegin mode
+                          body
+                          glEnd
+                          glFlush
 
+displayMe :: MonadIO m => m ()
+displayMe = do glClear GL_COLOR_BUFFER_BIT
+               withGLMode GL_POLYGON -- how we connect the points
+                          (do glVertex3f 0.0 0.0 0.0
+                              glVertex3f 0.5 0.0 0.0
+                              glVertex3f 0.5 0.5 0.0
+                              glVertex3f 0.0 0.5 0.0
+                              glVertex3f 0.0 0.0 0.0)
+
+window :: MonadIO m => CString -> m Window
 window s = createWindow s
                         SDL_WINDOWPOS_UNDEFINED
                         SDL_WINDOWPOS_UNDEFINED
@@ -41,29 +55,36 @@ window s = createWindow s
                         480
                         (SDL_WINDOW_SHOWN .|. SDL_WINDOW_OPENGL)
 
+eprintf :: String -> IO a
 eprintf s = putStr s *> exitFailure
 
-eventLoop :: IO Bool
+eventLoop :: MonadIO m => m Bool
 eventLoop = do e <- pollEvent
                case e of
                  Nothing -> return False
                  Just p -> case eventPayload p of
                              QuitEvent -> return True
                              _ -> return False
+
+appLoop :: MonadIO m => m ()
 appLoop = do delay 15
              glClear GL_COLOR_BUFFER_BIT
              displayMe
 
+main :: IO ()
 main = do c <- init SDL_INIT_VIDEO
           when (c < 0) exitFailure
           w <- withCString "SDL_Tutorial" window
           when (w == nullPtr)
-               (do s <- join (peekCString <$> getError)
+               (do p <- getError
+                   s <- peekCString p
                    putStrLn s
                    eprintf "Window could not be created!\n")
           context <- glCreateContext w
           when (context == nullPtr)
                (eprintf "OpenGL context could be created!\n")
+          -- Sets a global RBGA value, the clear color
+          -- This is green
           glClearColor 0 1 0 1
           whileM_ (not <$> eventLoop) (appLoop *> glSwapWindow w)
           glDeleteContext context
