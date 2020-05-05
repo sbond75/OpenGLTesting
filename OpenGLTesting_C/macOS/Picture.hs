5513b1d8210a65ff869fb15fd597ba59186e702e
Ben Siraphob <bensiraphob@gmail.com>
2020-05-05 07:59:40 +0700

    Generalize to continuous colors, add image result, transformations

--- OpenGLTesting_C/macOS/Picture.hs
+++ OpenGLTesting_C/macOS/Picture.hs
@@ -9,27 +9,37 @@ foreign export ccall calcR :: CInt -> CInt -> CUChar
 foreign export ccall calcG :: CInt -> CInt -> CUChar
 foreign export ccall calcB :: CInt -> CInt -> CUChar
 
-g :: (CInt, CInt) -> (Int, Int)
-g (x,y) = (fromIntegral x, fromIntegral y)
+calcWith f = fromIntegral . f . transform theImage . g
+  where
+    g (x,y) = (fromIntegral x, fromIntegral y)
+    transform = scale (2,2) . translate (100, 100)
+
 calcR :: CInt -> CInt -> CUChar
-calcR x y = fromIntegral $ f $ theImage $ g (x,y)
-  where f (Color (x,y,z)) = x
+calcR x y = calcWith f (x,y)
+  where
+    f (Color (x,_,_)) = floor x
 
 calcG :: CInt -> CInt -> CUChar
-calcG x y = fromIntegral $ f $ theImage $ g (x,y)
-  where f (Color (x,y,z)) = y
+calcG x y = calcWith f (x,y)
+  where
+    f (Color (_,y,_)) = floor y
 
 calcB :: CInt -> CInt -> CUChar
-calcB x y = fromIntegral $ f $ theImage $ g (x,y)
-  where f (Color (x,y,z)) = z
+calcB x y = calcWith f (x,y)
+  where
+    f (Color (_,_,z)) = floor z
 
---            R    G    B
-newtype Color = Color { unColor :: (Int, Int, Int) } deriving Show
--- instance 
-type Point = (Int, Int)
+--                                  R    G    B
+newtype Color = Color { unColor :: (Float, Float, Float) } deriving Show
 
-type Image = Point -> Color
+type Point = (Float, Float)
 
+type Image a = Point -> a
+type Region = Image Bool
+
+regionToImage :: Region -> Image Color
+regionToImage f = (\b -> if b then white else black) . f
+  
 instance Semigroup Color where
   Color (a,b,c) <> Color (x,y,z) = Color (a + x, b + y, c + z)
 
@@ -39,6 +49,12 @@ instance Monoid Color where
 black = Color (0,0,0)
 white = Color (255,255,255)
 
-theImage :: Image
-theImage (x,y) | x * x + y * y <= 10000 = white
-               | otherwise              = mempty
+theImage :: Image Color
+theImage (x,y) | x * x + y * y <= 10000 = Color (x + 20, y + 30, x + y)
+               | otherwise              = black
+
+translate :: Point -> Image a -> Image a
+translate (a,b) f = f . (\(x,y) -> (x-a,y-b))
+
+scale :: Point -> Image a -> Image a
+scale (a,b) f = f . (\(x,y) -> (x/a,y/b))
