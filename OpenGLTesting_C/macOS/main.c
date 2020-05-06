5f9385b1eb4a4829540bbf4a7ec0dbf2b5f2cec6
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-05-05 20:46:04 -0400

    added time, with diagonal movement

--- OpenGLTesting_C/macOS/main.c
+++ OpenGLTesting_C/macOS/main.c
@@ -17,123 +17,158 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 typedef struct Color {
-  Uint8 r, g, b
+	Uint8 r, g, b
 #ifdef USE_ALPHA
-      ,a
+	,a
 #endif
-      ;
+	;
 } Color;
 
-Uint8 calcA(int x, int y) {return 0;}
+Uint8 calcA(int x, int y, int t) { return 0; }
 
-Color funcFromPointToColor(int x, int y) {
-  return (Color) { .r = calcR(x,y), .g = calcG(x,y), .b = calcB(x,y)
+// Takes x and y positions, and time.
+Color funcFromPointToColor(int x, int y, int t) {
+  return (Color) { .r = calcR(x+t,y+t), .g = calcG(x+t,y+t), .b = calcB(x+t,y+t)
 #ifdef USE_ALPHA
-	, .a = calcA(x,y)
+	, .a = calcA(x+t,y+t,t)
 #endif
-        };
+   };
 }
 
 void drawPixel(int x, int y, SDL_Renderer* renderer, Color c) {
 #ifdef USE_ALPHA
-  const Uint8 alpha = c.a;
+	const Uint8 alpha = c.a;
 #else
-  const Uint8 alpha = SDL_ALPHA_OPAQUE;
+	const Uint8 alpha = SDL_ALPHA_OPAQUE;
 #endif
-  SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, alpha);
-  SDL_RenderDrawPoint(renderer, x, y);
+	SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, alpha);
+	SDL_RenderDrawPoint(renderer, x, y);
+}
+
+// t is time.
+void render(SDL_Renderer* renderer, SDL_Texture* screen, int t) {
+	// This line is only needed if we want to render over time,
+	// pixel by pixel and present between pixels.
+	//SDL_RenderClear(renderer);
+
+	// Render the pixels:
+	for (int i = 0; i < SCREEN_WIDTH; ++i) {
+		for (int j = 0; j < SCREEN_HEIGHT; ++j) {
+			Color c = funcFromPointToColor(i, j, t);
+			drawPixel(i, j, renderer, c);
+		}
+		/*
+		SDL_SetRenderTarget(renderer, NULL);
+		SDL_RenderCopy(renderer, screen, NULL, NULL);
+		SDL_RenderPresent(renderer);
+		SDL_SetRenderTarget(renderer, screen);
+		*/
+	}
+
+	SDL_SetRenderTarget(renderer, NULL);
+	SDL_RenderCopy(renderer, screen, NULL, NULL);
+	SDL_RenderPresent(renderer);
+	SDL_SetRenderTarget(renderer, screen);
+
+	// Present our pixels
+	SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char** argv)
 {
-  hs_init(&argc, &argv);
-  // The window we'll be rendering to
-  SDL_Window* window = NULL;
+	// Initialize the Haskell runtime system.
+    hs_init(&argc, &argv);
+	
+	// The window we'll be rendering to
+	SDL_Window* window = NULL;
 
-  // Renderer
-  SDL_Renderer* renderer = NULL;
+	// Renderer
+	SDL_Renderer* renderer = NULL;
 
-  // Main loop flag
-  bool quit = false;
+	// Main loop flag
+	bool quit = false;
 
-  // Event handler
-  SDL_Event e;
+	// Event handler
+	SDL_Event e;
 
-  // Initialize SDL
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    return 1;
-  }
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
 
-  // Create window
-  window = SDL_CreateWindow(NULL /*"SDL Tutorial"*/,
-                            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-                            SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN); 
-  if (window == NULL) {
-    fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 1;
-  }
+	// Create window
+	window = SDL_CreateWindow(NULL /*"SDL Tutorial"*/,
+							  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+						 	  SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN); 
+	if (window == NULL) {
+		fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
 
-  // Create renderer
-  renderer = SDL_CreateRenderer(window, -1, /*SDL_RENDERER_SOFTWARE*/ SDL_RENDERER_ACCELERATED);
-  if (renderer == NULL) {
-    fprintf(stderr, "Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 1;
-  }
+	// Create renderer
+	renderer = SDL_CreateRenderer(window, -1, /*SDL_RENDERER_SOFTWARE*/ SDL_RENDERER_ACCELERATED);
+	if (renderer == NULL) {
+		fprintf(stderr, "Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
 
-#ifdef USE_ALPHA
-  // Disable alpha blending for performance.
-  SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
+	SDL_Texture* screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	// Make the renderer render to a texture:
+	SDL_SetRenderTarget(renderer, screen);
+
+#ifndef USE_ALPHA
+	// Disable alpha blending for performance.
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
 #endif
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
 
-  // Render the pixels:
-  for (int i = 0; i < SCREEN_WIDTH; ++i) {
-    for (int j = 0; j < SCREEN_HEIGHT; ++j) {
-      Color c = funcFromPointToColor(i, j);
-      drawPixel(i, j, renderer, c);
-    }
-  }
+	int t = 0;
+	render(renderer, screen, t);
 
-  // Present our pixels
-  SDL_RenderPresent(renderer);
+	// While application is running
+	while (!quit) {
+		// Handle events on queue
+		while (SDL_PollEvent(&e) != 0)
+		{
+			// User requests quit
+			Sint32 k;
+			Uint32 ticksBefore, ticksAfter;
+			switch (e.type) {
+			case SDL_KEYDOWN:
+				k = e.key.keysym.sym;
+				if (k == SDLK_r) {
+					// Redraw
+					ticksBefore = SDL_GetTicks();
+					SDL_RenderPresent(renderer);
+					ticksAfter = SDL_GetTicks();
+					printf("Time elapsed for redraw: %d\n", ticksAfter - ticksBefore);
+					fflush(stdout);
+				}
+				break;
+			case SDL_KEYUP:
+				//Sint32 k = e.key.keysym.sym;
+				break;
+			case SDL_QUIT:
+				quit = true;
+				break;
+			}
+		}
 
-  // While application is running
-  while (!quit) {
-    // Handle events on queue
-    while (SDL_PollEvent(&e) != 0)
-    {
-      // User requests quit
-      Sint32 k;
-      Uint32 ticksBefore, ticksAfter;
-      switch (e.type) {
-        case SDL_KEYDOWN:
-          k = e.key.keysym.sym;
-          if (k == SDLK_r) {
-            // Redraw
-            ticksBefore = SDL_GetTicks();
-            SDL_RenderPresent(renderer);
-            ticksAfter = SDL_GetTicks();
-            printf("Time elapsed for redraw: %d\n", ticksAfter - ticksBefore);
-            fflush(stdout);
-          }
-          break;
-        case SDL_KEYUP:
-          //Sint32 k = e.key.keysym.sym;
-          break;
-        case SDL_QUIT:
-          quit = true;
-          break;
-      }
-    }
+		// Delay
+		//SDL_Delay(15);
 
-    // Delay
-    SDL_Delay(15);
-  }
+		// Render
+		t++;
+		render(renderer, screen, t);
+	}
 
-  /* Destroy our renderer, destroy our window, and shutdown SDL */
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  hs_exit();
-  return 0;
+	/* Destroy our renderer, destroy our window, and shutdown SDL */
+	SDL_DestroyTexture(screen);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+    hs_exit();
+    return 0;
 }
